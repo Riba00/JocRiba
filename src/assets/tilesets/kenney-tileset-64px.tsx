2faf4ce1b4c2fddb2651cb6fe872541ff48c530b
Riba00 <jordiriba@iesebre.com>
2023-05-17 17:25:55 +0200

    falta game over portada i victoria

---
+++ src/assets/tilesets/kenney-tileset-64px.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="kenney-tileset-64px" tilewidth="64" tileheight="64" tilecount="374" columns="22">
+ <image source="../../../../Phaser3Platformer/assets/img/kenney-tileset-64px.png" width="1408" height="1088"/>
+</tileset>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 
+let score = 0;
+let scoreText;
+let scoreImage
+let lives = 3;
+let livesText;
+
+
 // Escena 1
 class Scene1 extends Phaser.Scene {
+
     constructor() {
         super({key: 'Scene1'});
     }
@@ -10,30 +18,72 @@ class Scene1 extends Phaser.Scene {
     preload() {
         this.load.image('background', 'src/assets/images/background.png');
         this.load.image('spike', 'src/assets/images/spike.png');
+        this.load.image('seta', 'src/assets/images/seta.png');
+        this.load.image('enemy', 'src/assets/images/enemy.png');
+
         // At last image must be loaded with its JSON
         this.load.atlas('player', 'src/assets/images/kenney_player.png', 'src/assets/images/kenney_player_atlas.json');
         this.load.image('tiles', 'src/assets/tilesets/platformPack_tilesheet.png');
         // Load the export Tiled JSON
         this.load.tilemapTiledJSON('map', 'src/assets/tilemaps/level1.json');
+
+        this.load.spritesheet('star', 'src/assets/images/star.png', {frameWidth: 48, frameHeight: 48})
     }
 
     create() {
-        const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
-        backgroundImage.setScale(2, 0.8);
-
+        // Create a tile map, which is used to bring our level in Tiled
+        // to our game world in Phaser
         const map = this.make.tilemap({key: 'map'});
-
+        // Add the tileset to the map so the images would load correctly in Phaser
         const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
-
-        const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
-
+        // Place the background image in our game world
+        const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        // Scale the image to better match our game's resolution
+        backgroundImage.setScale(2, 0.8);
+        // Add the platform layer as a static group, the player would be able
+        // to jump on platforms like world collisions but they shouldn't move
+        const platforms = map.createLayer('Platforms', tileset, 0, 200);
+        const stars = map.createLayer('Stars', tileset, 0, 200);
+        // There are many ways to set collision between tiles and players
+        // As we want players to collide with all of the platforms, we tell Phaser to
+        // set collisions for every tile in our platform layer whose index isn't -1.
+        // Tiled indices can only be >= 0, therefore we are colliding with all of
+        // the platform layer
         platforms.setCollisionByExclusion(-1, true);
 
-        this.player = this.physics.add.sprite(50, 300, 'player');
-        this.player.setBounce(0.1);
-        this.player.setCollideWorldBounds(true);
+        this.star1 = this.physics.add.sprite(400, 550, 'star')
+        this.physics.add.collider(this.star1, platforms)
+
+
+        this.star2 = this.physics.add.sprite(550, 300, 'star')
+        this.physics.add.collider(this.star2, platforms)
+
+        this.star3 = this.physics.add.sprite(1175, 300, 'star')
+        this.physics.add.collider(this.star3, platforms)
+
+        this.seta = this.physics.add.sprite(1560, 300, 'seta')
+        this.physics.add.collider(this.seta, platforms)
+
+        this.enemy = this.physics.add.sprite(750, 200, 'enemy')
+        this.physics.add.collider(this.enemy, platforms)
+
+
+
+        // Add the player to the game world
+        this.player = this.physics.add.sprite(50, 525, 'player');
+        this.player.setBounce(0.1); // our player will bounce from items
+        this.player.setCollideWorldBounds(true); // don't go out of the map
         this.physics.add.collider(this.player, platforms);
 
+        // set bounds so the camera won't go outside the game world
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        // make the camera follow the player
+        this.cameras.main.startFollow(this.player);
+        this.physics.world.bounds.width = platforms.width;
+        this.physics.world.bounds.height = 640;
+
+        // Create the walking animation using the last 2 frames of
+        // the atlas' first row
         this.anims.create({
             key: 'walk',
             frames: this.anims.generateFrameNames('player', {
@@ -45,19 +95,88 @@ class Scene1 extends Phaser.Scene {
             repeat: -1
         });
 
+        // Create an idle animation i.e the first frame
         this.anims.create({
             key: 'idle',
             frames: [{key: 'player', frame: 'robo_player_0'}],
             frameRate: 10,
         });
 
+        // Use the second frame of the atlas for jumping
         this.anims.create({
             key: 'jump',
             frames: [{key: 'player', frame: 'robo_player_1'}],
             frameRate: 10,
         });
 
+        // Enable user input via cursor keys
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Create a sprite group for all spikes, set common properties to ensure that
+        // sprites in the group don't move via gravity or by player collisions
+        this.spikes = this.physics.add.group({
+            allowGravity: false,
+            immovable: true
+        });
+
+        // Get the spikes from the object layer of our Tiled map. Phaser has a
+        // createFromObjects function to do so, but it creates sprites automatically
+        // for us. We want to manipulate the sprites a bit before we use them
+        map.getObjectLayer('Spikes').objects.forEach((spike) => {
+            // Add new spikes to our sprite group
+            const spikeSprite = this.spikes.create(spike.x, spike.y + 200 - spike.height, 'spike').setOrigin(0);
+            // By default the sprite has loads of whitespace from the base image, we
+            // resize the sprite to reduce the amount of whitespace used by the sprite
+            // so collisions can be more precise
+            spikeSprite.body.setSize(spike.width, spike.height - 20).setOffset(0, 20);
+        });
+
+        // Add collision between the player and the spikes
+        this.physics.add.collider(this.player, this.spikes, playerHit, null, this);
+        this.physics.add.overlap(this.player, this.enemy, playerHit, null, this);
+
+
+
+        let scoreImage = this.add.image(10, 10, 'star');
+        scoreImage.setOrigin(0, 0);
+        scoreImage.setScale(0.5);
+        scoreText = this.add.text(70, 10, score.toString(), {
+            fontFamily: 'Arial',
+            fontSize: '24px',
+            fill: '#000000'
+        });
+        scoreText.setOrigin(1, 0);
+        scoreText.setScrollFactor(0);
+        scoreImage.setScrollFactor(0);
+
+        livesText = this.add.text(100, 10, 'Lifes: ' + lives, {fontSize: '24px', fill: '#000000'});
+        livesText.setScrollFactor(0);
+
+
+        this.physics.add.overlap(this.player, this.star1, function (player, star1) {
+            incrementScore(star1);
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.star2, function (player, star2) {
+            incrementScore(star2);
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.star3, function (player, star3) {
+            incrementScore(star3);
+        }, null, this);
+        this.physics.add.overlap(this.player, this.seta, level1ToLevel2, null, this);
+
+        function incrementScore(sprite) {
+            score++;
+            scoreText.setText(score.toString());
+            scoreImage.setTexture('star');
+            scoreImage.setScale(0.5);
+            // sound_coin.play();
+
+            sprite.destroy();
+        }
+
+
     }
 
     update() {
@@ -81,8 +200,8 @@ class Scene1 extends Phaser.Scene {
             }
         }
 
-// Player can jump while walking any direction by pressing the space bar
-// or the 'UP' arrow
+        // Player can jump while walking any direction by pressing the space bar
+        // or the 'UP' arrow
         if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
             this.player.setVelocityY(-350);
             this.player.play('jump', true);
@@ -97,6 +216,7 @@ class Scene1 extends Phaser.Scene {
 
 
     }
+
 }
 
 // Escena 2
@@ -106,15 +226,213 @@ class Scene2 extends Phaser.Scene {
     }
 
     preload() {
-        // Carga los recursos necesarios para la escena 2
+        this.load.image('background2', 'src/assets/images/background.png');
+        this.load.image('spike2', 'src/assets/images/spike.png');
+        this.load.image('seta2', 'src/assets/images/seta.png');
+        this.load.image('enemy2', 'src/assets/images/enemy.png');
+
+        // At last image must be loaded with its JSON
+        this.load.atlas('player', 'src/assets/images/kenney_player.png', 'src/assets/images/kenney_player_atlas.json');
+        this.load.image('tiles', 'src/assets/tilesets/platformPack_tilesheet.png');
+        // Load the export Tiled JSON
+        this.load.tilemapTiledJSON('map2', 'src/assets/tilemaps/level2.json');
+
+        this.load.spritesheet('star2', 'src/assets/images/star.png', {frameWidth: 48, frameHeight: 48})
     }
 
     create() {
-        // Configuración inicial de la escena 2
+        // Create a tile map, which is used to bring our level in Tiled
+        // to our game world in Phaser
+        const map = this.make.tilemap({key: 'map2'});
+        // Add the tileset to the map so the images would load correctly in Phaser
+        const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
+        // Place the background image in our game world
+        const backgroundImage = this.add.image(0, 0, 'background2').setOrigin(0, 0);
+        // Scale the image to better match our game's resolution
+        backgroundImage.setScale(2, 0.8);
+        // Add the platform layer as a static group, the player would be able
+        // to jump on platforms like world collisions but they shouldn't move
+        const platforms = map.createLayer('Platforms', tileset, 0, 200);
+        const stars = map.createLayer('Stars', tileset, 0, 200);
+        // There are many ways to set collision between tiles and players
+        // As we want players to collide with all of the platforms, we tell Phaser to
+        // set collisions for every tile in our platform layer whose index isn't -1.
+        // Tiled indices can only be >= 0, therefore we are colliding with all of
+        // the platform layer
+        platforms.setCollisionByExclusion(-1, true);
+
+        this.star1 = this.physics.add.sprite(400, 300, 'star')
+        this.physics.add.collider(this.star1, platforms)
+
+
+        this.star2 = this.physics.add.sprite(550, 400, 'star')
+        this.physics.add.collider(this.star2, platforms)
+
+        this.star3 = this.physics.add.sprite(1175, 300, 'star')
+        this.physics.add.collider(this.star3, platforms)
+
+        this.star4 = this.physics.add.sprite(1000, 200, 'star')
+        this.physics.add.collider(this.star4, platforms)
+
+        this.seta = this.physics.add.sprite(1560, 300, 'seta')
+        this.physics.add.collider(this.seta, platforms)
+
+        this.enemy = this.physics.add.sprite(850, 200, 'enemy')
+        this.physics.add.collider(this.enemy, platforms)
+
+
+
+        // Add the player to the game world
+        this.player = this.physics.add.sprite(50, 525, 'player');
+        this.player.setBounce(0.1); // our player will bounce from items
+        this.player.setCollideWorldBounds(true); // don't go out of the map
+        this.physics.add.collider(this.player, platforms);
+
+        // set bounds so the camera won't go outside the game world
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        // make the camera follow the player
+        this.cameras.main.startFollow(this.player);
+        this.physics.world.bounds.width = platforms.width;
+        this.physics.world.bounds.height = 640;
+
+        // Create the walking animation using the last 2 frames of
+        // the atlas' first row
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNames('player', {
+                prefix: 'robo_player_',
+                start: 2,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        // Create an idle animation i.e the first frame
+        this.anims.create({
+            key: 'idle',
+            frames: [{key: 'player', frame: 'robo_player_0'}],
+            frameRate: 10,
+        });
+
+        // Use the second frame of the atlas for jumping
+        this.anims.create({
+            key: 'jump',
+            frames: [{key: 'player', frame: 'robo_player_1'}],
+            frameRate: 10,
+        });
+
+        // Enable user input via cursor keys
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Create a sprite group for all spikes, set common properties to ensure that
+        // sprites in the group don't move via gravity or by player collisions
+        this.spikes = this.physics.add.group({
+            allowGravity: false,
+            immovable: true
+        });
+
+        // Get the spikes from the object layer of our Tiled map. Phaser has a
+        // createFromObjects function to do so, but it creates sprites automatically
+        // for us. We want to manipulate the sprites a bit before we use them
+        map.getObjectLayer('Spikes').objects.forEach((spike) => {
+            // Add new spikes to our sprite group
+            const spikeSprite = this.spikes.create(spike.x, spike.y + 200 - spike.height, 'spike').setOrigin(0);
+            // By default the sprite has loads of whitespace from the base image, we
+            // resize the sprite to reduce the amount of whitespace used by the sprite
+            // so collisions can be more precise
+            spikeSprite.body.setSize(spike.width, spike.height - 20).setOffset(0, 20);
+        });
+
+        // Add collision between the player and the spikes
+        this.physics.add.collider(this.player, this.spikes, playerHit, null, this);
+        this.physics.add.overlap(this.player, this.enemy, playerHit, null, this);
+
+
+
+        let scoreImage = this.add.image(10, 10, 'star');
+        scoreImage.setOrigin(0, 0);
+        scoreImage.setScale(0.5);
+        scoreText = this.add.text(70, 10, score.toString(), {
+            fontFamily: 'Arial',
+            fontSize: '24px',
+            fill: '#000000'
+        });
+        scoreText.setOrigin(1, 0);
+        scoreText.setScrollFactor(0);
+        scoreImage.setScrollFactor(0);
+
+        livesText = this.add.text(100, 10, 'Lifes: ' + lives, {fontSize: '24px', fill: '#000000'});
+        livesText.setScrollFactor(0);
+
+
+        this.physics.add.overlap(this.player, this.star1, function (player, star1) {
+            incrementScore(star1);
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.star2, function (player, star2) {
+            incrementScore(star2);
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.star3, function (player, star3) {
+            incrementScore(star3);
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.star4, function (player, star4) {
+            incrementScore(star4);
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.seta, level1ToLevel2, null, this);
+
+        function incrementScore(sprite) {
+            score++;
+            scoreText.setText(score.toString());
+            scoreImage.setTexture('star');
+            scoreImage.setScale(0.5);
+            // sound_coin.play();
+
+            sprite.destroy();
+        }
+
+
+
     }
 
     update() {
-        // Lógica de actualización de la escena 2
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-200);
+            if (this.player.body.onFloor()) {
+                this.player.play('walk', true);
+            }
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(200);
+            if (this.player.body.onFloor()) {
+                this.player.play('walk', true);
+            }
+        } else {
+            // If no keys are pressed, the player keeps still
+            this.player.setVelocityX(0);
+            // Only show the idle animation if the player is footed
+            // If this is not included, the player would look idle while jumping
+            if (this.player.body.onFloor()) {
+                this.player.play('idle', true);
+            }
+        }
+
+        // Player can jump while walking any direction by pressing the space bar
+        // or the 'UP' arrow
+        if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
+            this.player.setVelocityY(-350);
+            this.player.play('jump', true);
+        }
+
+        if (this.player.body.velocity.x > 0) {
+            this.player.setFlipX(false);
+        } else if (this.player.body.velocity.x < 0) {
+            // otherwise, make them face the other side
+            this.player.setFlipX(true);
+        }
+
     }
 }
 
@@ -160,16 +478,57 @@ class Scene4 extends Phaser.Scene {
 const config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 645,
     scene: [Scene1, Scene2, Scene3, Scene4],
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 400 },
+            gravity: {y: 400},
             debug: false
         }
     },
 };
+
+function playerHit(player) {
+    if (player.y+100 < this.enemy.y){
+        this.enemy.destroy()
+    } else{
+        this.physics.pause();
+        player.setY(this.player.y - 500);
+        lives--
+        livesText.setText('Lifes: ' + lives);
+        // Set velocity back to 0
+        player.setVelocity(0, 0);
+        // Put the player back in its original position
+        player.setX(50);
+        player.setY(525);
+        // Use the default `idle` animation
+        player.play('idle', true);
+        // Set the visibility to 0 i.e. hide the player
+        player.setAlpha(0);
+        // Add a tween that 'blinks' until the player is gradually visible
+        let tw = this.tweens.add({
+            targets: player,
+            alpha: 1,
+            duration: 100,
+            ease: 'Linear',
+            repeat: 5,
+        });
+        setTimeout(() => {
+            this.physics.resume();
+        }, 600);
+    }
+}
+
+function level1ToLevel2() {
+    if (score >= 3){
+        this.scene.start('Scene2');
+    }
+}
+
+
+
+
 
 // Creación del juego
 const game = new Phaser.Game(config);
